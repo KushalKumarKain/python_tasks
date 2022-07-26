87ebc7cd5bade1f5016ce89f9859a49682f6cdac
kushalkain <kushalkumar.kain156@gmail.com>
2022-07-26 12:28:36 +0530

    [UPDATED]code for error handling

--- Assignment5_eval/operations/case8.py
+++ Assignment5_eval/operations/case8.py
@@ -6,13 +6,35 @@ toc = int(input('''Choose from
                 2 = Energy Charges
                 '''))
 
-overallUnits = int(input("Enter the amount of units used : "))
-if toc==1:
-	
-	result=(overallUnits*25)
-	print("The rare here is FIXED and it is 340 Paisa/unit, so your bill comes out to be Rs", result)
+class Eight():
+  def run(overallUnits):
 
-if toc==2:
+    if toc==1:
+      overallUnits = int(input("Enter the amount of units used : "))
+      result=(overallUnits*25)
+      print("The rare here is FIXED and it is 340 Paisa/unit, so your bill comes out to be Rs", result)
 
-	result=(overallUnits*420)/100
-	print("There is only Flat Phase, and the rate of it is 340 Paisa/unit, so your bill comes out to be Rs", result)
+    elif toc==2:
+      overallUnits = int(input("Enter the amount of units used : "))
+      result=(overallUnits*420)/100
+      print("There is only Flat Phase, and the rate of it is 340 Paisa/unit, so your bill comes out to be Rs", result)
+      
+    else:
+      class ErrorClass(Exception):
+        """For all exceptions"""
+        pass
+      class NumberError(ErrorClass):
+        """Raised when toc input is invalid"""
+        pass
+      try:
+        if toc != 1 | 2 :
+          raise NumberError 
+        elif type(toc) != int:
+          raise NumberError
+        
+      except NumberError:
+        print("Invalid input")
+          
+        
+
+Eight.run(toc)
