7dc2b05ac516caacfaf4c29c064db981849963e0
kushalkain <kushalkumar.kain156@gmail.com>
2022-07-26 12:16:15 +0530

    [UPDATED] code to oops and error handling.

--- Assignment5_eval/operations/case7.py
+++ Assignment5_eval/operations/case7.py
@@ -1,17 +1,25 @@
+print("")
 print("You have chosen 7. SL : Low Tension Tension Street Light Service")
 
 overallUnits = int(input("Enter the amount of units used : "))
 
-def seven():
-  result=(overallUnits*430)/100
+class Seven():
 
-class ErrorSeven:
-  
+  def run(overallUnits):
+    if overallUnits>0:
+      result=(overallUnits*430)/100
+      print('''
+            
+            There is only Flat Phase, and the rate of it is 340 Paisa/unit, so your bill comes out to be Rs ''',result)
+    else:
+      if overallUnits<0:
+        class ErrorClass(Exception):
+            """For all exceptions"""
+            pass
+        class NumberError(ErrorClass):
+          """Raised when toc input is invalid"""
+          pass
+        print("Invalid input!!")
+        raise NumberError
 
-
-  
-if overallUnits<0:
-  raise Error
-  print("Invalid input!!")
-else:
-  print("There is only Flat Phase, and the rate of it is 340 Paisa/unit, so your bill comes out to be Rs ", seven(result))
+Seven.run(overallUnits)
